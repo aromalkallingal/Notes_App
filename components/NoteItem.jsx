50d5663a33e1637156e7ff48ee2a7f231362fd6b
aromalkallingal5@gmail.com <aromalkallingal5@gmail.com>
2025-04-27 12:44:29 +0530

    adding update button

--- components/NoteItem.jsx
+++ components/NoteItem.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useState, useRef } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 function NoteItem({ note, onDelete }) {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(note.text);
+  const inputRef = useRef(null);
+
   return (
     <View style={styles.noteItem}>
+      { isEditing ? (
+        <TextInput 
+          ref={inputRef}
+          style={styles.input}
+          value={editedText}
+          onChangeText={setEditedText}
+          autoFocus
+          onSubmitEditing={handleSave}
+          returnKeyType='done'
+
+        />
+      ): (
         <Text style={styles.noteText}>{note.text}</Text>
+      ) }
+
+        
         <TouchableOpacity onPress={()=> onDelete(note.$id)}>
             <Text style={styles.delete}>X</Text>
         </TouchableOpacity>
