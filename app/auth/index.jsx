56c3ec9a59e476fbae33fae32cee9b05a203f595
aromalkallingal5@gmail.com <aromalkallingal5@gmail.com>
2025-05-11 19:43:22 +0530

    Added Auth index

---
+++ app/auth/index.jsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from "react-native";
+
+
+const AuthScreen = () => {
+  return (<View style={styles.container}>
+    <Text></Text>
+    </View> );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#ffff",
+
+  }
+})
+
+export default AuthScreen;
